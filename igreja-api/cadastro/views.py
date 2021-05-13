00f3fd858afc38aec6249803262f2d91f55588ed
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-12 22:37:40 -0300

    Criação viewset Membro

---
+++ igreja-api/cadastro/views.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ModelViewSet
+
+from igreja.models import Membro
+
+from cadastro.serializers import MembroSerializer
+
+class MembroViewSet(ModelViewSet):
+    queryset = Membro.objects.all()
+    serializer_class =  MembroSerializer
+
+    http_method_names = ['get', 'post', 'put']
