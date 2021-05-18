97eee3a429469c33cb04fe4c416af74e85e249d8
1-aquila-1 <aquilla.tavares7@gmail.com>
2021-05-17 23:00:10 -0300

    Pessoa

---
+++ igreja-api/igreja/src/main/java/br/com/gestao/igreja/model/Pessoa.java
@@ -0,0 +1,24 @@
+package br.com.gestao.igreja.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;    
+}
